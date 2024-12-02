ddb50aff755339f89e21a49d9f6e55f4061e17e2
Alex Kim <alexkkim123@gmail.com>
2024-12-02 16:47:35 -0700

    Tightening it up a bit

--- 2/main.py
+++ 2/main.py
@@ -10,16 +10,16 @@ def level_satisfies_checks(current_level, next_level, should_ascend):
     level_difference_in_range = True if 0 < abs(next_level-current_level) <= 3 else False
     return all([maintains_direction, level_difference_in_range])
 
-def find_first_two_potentially_problematic_levels(report):
+def find_first_potentially_problematic_levels(report):
     """
     Determines if a report is safe
     :param report: The report to verify
-    :return: List of first two detected potentially problematic levels, if any
+    :return: List of first two-three detected potentially problematic levels, if any
     """
     should_ascend = True if report[1] > report[0] else False  # set initial direction
     for level in range(len(report) - 1):
         if not level_satisfies_checks(report[level], report[level + 1], should_ascend):
-            return [level, level + 1]
+            return [level - 1, level, level + 1] if level > 0 else [level, level + 1]
     return []
 
 #Part 1
@@ -27,7 +27,7 @@ def find_first_two_potentially_problematic_levels(report):
 num_safe_reports = 0
 
 for report in reports:
-    if len(find_first_two_potentially_problematic_levels(report)) == 0:
+    if len(find_first_potentially_problematic_levels(report)) == 0:
         num_safe_reports = num_safe_reports + 1
 
 print(num_safe_reports)
@@ -41,22 +41,17 @@ def pop_and_remove(arr, index):
 num_safe_reports = 0
 
 for report in reports:
-    first_two_problematic_levels = find_first_two_potentially_problematic_levels(report)
-    if len(first_two_problematic_levels) == 0:
+    first_problematic_levels = find_first_potentially_problematic_levels(report)
+    if len(first_problematic_levels) == 0:
         num_safe_reports = num_safe_reports + 1
     else:
         #Try removing each suggested problematic level and see if the report is safe
-        first_dampen_try_did_succeed = len(find_first_two_potentially_problematic_levels(
-            pop_and_remove(report.copy(), first_two_problematic_levels[0]))) == 0
-        second_dampen_try_did_succeed = len(find_first_two_potentially_problematic_levels(
-            pop_and_remove(report.copy(), first_two_problematic_levels[1]))) == 0
-        if first_two_problematic_levels[0] > 0:
-            backwards_dampen_try_did_succeed = len(find_first_two_potentially_problematic_levels(
-                pop_and_remove(report.copy(), first_two_problematic_levels[0]-1))) == 0
-            if backwards_dampen_try_did_succeed or first_dampen_try_did_succeed or second_dampen_try_did_succeed:
-                num_safe_reports = num_safe_reports + 1
-        else:
-            if first_dampen_try_did_succeed or second_dampen_try_did_succeed:
-                num_safe_reports = num_safe_reports + 1
+        level_removal_tries = []
+        for level_index in first_problematic_levels:
+            level_removal_tries.append(
+                len(find_first_potentially_problematic_levels(pop_and_remove(report.copy(), level_index)))==0
+            )
+        if any(level_removal_tries):
+           num_safe_reports = num_safe_reports + 1
 
 print(num_safe_reports)
